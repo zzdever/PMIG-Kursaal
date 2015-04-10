d4d8b449d6d2d4bcc71aad7e937e47a30b161351
zzdever <zzdever@gmail.com>
2015-04-10 14:32:22 +0800

    math done

--- p2dengine/general/p2dparams.h
+++ p2dengine/general/p2dparams.h
@@ -22,6 +22,22 @@
 /// The maximum number of vertices on a convex polygon.
 #define P2D_MAX_POLYGON_VERTICES 8
 
+/// A small length used as a collision and constraint tolerance. Usually it is
+/// chosen to be numerically significant, but visually insignificant.
+#define P2D_LINEAR_SLOP 0.005f
+
+/// A small angle used as a collision and constraint tolerance. Usually it is
+/// chosen to be numerically significant, but visually insignificant.
+#define P2D_ANGULAR_SLOP (2.0f / 180.0f * PI)
+
+/// The radius of the polygon/edge shape skin. This should not be modified. Making
+/// this smaller means polygons will have an insufficient buffer for continuous collision.
+/// Making it larger may create artifacts for vertex collision.
+#define P2D_POLYGON_RADIUS (2.0f * P2D_LINEAR_SLOP)
+
+/// Maximum number of sub-steps per contact in continuous physics simulation.
+#define P2D_MAX_SUB_STEPS 8
+
 
 
 typedef unsigned char uint8;
