79da259838c2e6224027aaff48651e4cc7190412
zzdever <zzdever@gmail.com>
2015-04-10 15:58:23 +0800

    draw new shape done

--- scenemanager.cpp
+++ scenemanager.cpp
@@ -2,6 +2,8 @@
 
 SceneManager::SceneManager()
 {
+    isDrawing = false;
+
     QColor color(100,100,100);
     item = new Chip(color, 0, 0);
     item->setPos(QPointF(0, 0));
@@ -62,10 +64,85 @@ void SceneManager::Render()
 
 void SceneManager::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-    qDebug()<<"MyGraphicsScene"<<event->pos();
-    qDebug()<<"MyGraphicsScene scene"<<event->scenePos();
-    qDebug()<<"MyGraphicsScene screen"<<event->screenPos();
+    // We can only draw in a blank area.
+    if(itemAt(event->scenePos(), QTransform()) == nullptr){
+        isDrawing = true;
+        drawingItem = new DrawingPolygon(QColor(qrand()%255, qrand()%255, qrand()%255), event->scenePos());
+        //drawingItem->setPos(event->scenePos());
+        addItem(drawingItem);
+    }
 
     QGraphicsScene::mousePressEvent(event);
 }
+
+
+void SceneManager::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(isDrawing){
+        drawingItem->AddPoint(event->scenePos());
+        update();
+    }
+
+    QGraphicsScene::mouseMoveEvent(event);
+}
+
+
+void SceneManager::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(isDrawing){
+        isDrawing = false;
+        removeItem(drawingItem);
+
+        // TODO add to the object tree
+    }
+
+    QGraphicsScene::mouseReleaseEvent(event);
+}
+
+
+
+
+DrawingPolygon::DrawingPolygon(QColor color, QPointF p)
+{
+    points.push_back(p);
+    this->color = color;
+
+    // This should be maximized, put it on the top of everything.
+    setZValue(100);
+}
+
+void DrawingPolygon::AddPoint(QPointF p)
+{
+    points.push_back(p);
+}
+
+QRectF DrawingPolygon::boundingRect() const
+{
+    return QRectF(0, 0, 0, 0);
+}
+
+void DrawingPolygon::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
+
+    // draw the forming polygon
+    // size is not very large, <100 for a not crazily complex polygon
+    if (points.size() > 1) {
+        QPen p = painter->pen();
+        QBrush b = painter->brush();
+        painter->setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+        painter->setBrush(QBrush(color, Qt::Dense7Pattern));
+        QPainterPath path;
+        path.moveTo(points.first());
+        for (int i = 1; i < points.size(); ++i)
+            path.lineTo(points.at(i));
+        painter->drawPath(path);
+        painter->setPen(p);
+        painter->setBrush(b);
+    }
+}
+
+
+
 
--- scenemanager.h
+++ scenemanager.h
@@ -8,6 +8,8 @@
 
 #include "playground.h"
 
+class DrawingPolygon;
+
 class SceneManager : public QGraphicsScene{
 public:
     SceneManager();
@@ -16,12 +18,40 @@ public:
 private:
     QGraphicsItem *item;
 
+    bool isDrawing;
+    DrawingPolygon *drawingItem;
+
     int tmp;
 
 protected:
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 
 };
+
+
+
+#include <QColor>
+#include <QGraphicsItem>
+#include <QtWidgets>
+
+class DrawingPolygon : public QGraphicsItem
+{
+public:
+    DrawingPolygon(QColor color, QPointF p);
+    void AddPoint(QPointF p);
+
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget);
+
+
+private:
+    QColor color;
+    QVector<QPointF> points;
+};
+
+
 
 
 #endif // SCENEMANAGER_H
